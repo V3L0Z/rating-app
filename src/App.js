0215a940b1f229ea37d4f2269bc69504d3435ea4
Gabriel Veloz <gabriel.veloz@live.com>
2021-06-12 09:28:54 -0500

    complete project added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Star from './Star'
 
 function App() {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  const stars = [1, 2, 3, 4, 5];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} class="App-logo" />
+        <h1>Star Rating</h1>
+        <div class="flex-container">
+          {stars.map((star, index) => (
+            <Star 
+              key={index} 
+              starId={index} 
+              rating={hover || rating} 
+              onMouseEnter={() => setHover(index)} 
+              onMouseLeave={() => setHover(0)} 
+              onClick={() => setRating(index)}
+            />
+          ))}
+        </div>
       </header>
     </div>
   );
